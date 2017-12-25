0ba74ea9c96159e004562dfcacfc05774e1c8f0e
appi147 <arpitkumar147@gmail.com>
2017-12-25 16:12:53 +0530

    Refactoring of code
    Functions are broken down into smaller functions so that they can be easily debugged and reused
    such as parse_page is defined so that when top torrents is implemented this function can be used to parse that page also.
    Thank You.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+'''
+This is the main module
+'''
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify, render_template
@@ -9,63 +12,102 @@ EMPTY_LIST = []
 
 @APP.route('/', methods=['GET'])
 def index():
+    '''
+    This is the home page and contains links to other API
+    '''
     return render_template('index.html'), 200
 
 
 @APP.route('/recent/', methods=['GET'])
 @APP.route('/recent/<int:page>/', methods=['GET'])
 def recent_torrents(page=0):
+    '''
+    This function implements recent page of TPB
+    '''
     url = 'https://thepiratebay.org/recent/' + str(page)
+    return jsonify(parse_page(url)), 200
+
+
+def parse_page(url):
+    '''
+    This function parses the page and returns list of torrents
+    '''
     data = requests.get(url).text
     soup = BeautifulSoup(data, 'lxml')
-    # title of torrents
-    titleList = soup.find_all(class_='detLink')
-    titles = []
-    for title in titleList:
-        titles.append(title.get_text())
-    # magnet links of torrents
-    magnetL = soup.find('table', {'id': 'searchResult'})
-    # If page has no links or page is out of bounds
-    if magnetL is None:
-        return jsonify(EMPTY_LIST), 200
-    magnetList = magnetL.find_all('a', href=True)
+    table_present = soup.find('table', {'id': 'searchResult'})
+    if table_present is None:
+        return EMPTY_LIST
+    titles = parse_titles(soup)
+    magnets = parse_magnet_links(soup)
+    times, sizes, uploaders = parse_description(soup)
+    seeders, leechers = parse_seed_leech(soup)
+    torrents = []
+    for torrent in zip(titles, magnets, times, sizes, uploaders, seeders, leechers):
+        torrents.append({
+            'title': torrent[0],
+            'magnet': torrent[1],
+            'time': torrent[2],
+            'size': torrent[3],
+            'uploader': torrent[4],
+            'seeds': torrent[5],
+            'leechs': torrent[6],
+        })
+    return torrents
+
+
+def parse_magnet_links(soup):
+    '''
+    Returns list of magnet links from soup
+    '''
+    magnet_list = soup.find('table', {'id': 'searchResult'}).find_all('a', href=True)
     magnets = []
-    for magnet in magnetList:
+    for magnet in magnet_list:
         if 'magnet' in magnet['href']:
             magnets.append(magnet['href'])
-    # time, size and uploader
+    return magnets
+
+
+def parse_titles(soup):
+    '''
+    Returns list of titles of torrents from soup
+    '''
+    title_list = soup.find_all(class_='detLink')
+    titles = []
+    for title in title_list:
+        titles.append(title.get_text())
+    return titles
+
+
+def parse_description(soup):
+    '''
+    Returns list of time, size and uploader from soup
+    '''
+    description = soup.find_all('font', class_='detDesc')
     times = []
     sizes = []
     uploaders = []
-    description = soup.find_all('font', class_='detDesc')
     for desc in description:
         temp = desc.get_text().split(',')
         times.append(temp[0].replace(u'\xa0', u' ').replace('Uploaded ', ''))
         sizes.append(temp[1].replace(u'\xa0', u' ').replace(' Size ', ''))
         uploaders.append(temp[2].replace(' ULed by ', ''))
-    # seeders and leechers
+    return times, sizes, uploaders
+
+
+def parse_seed_leech(soup):
+    '''
+    Returns list of numbers of seeds and leechs from soup
+    '''
     slinfo = soup.find_all('td', {'align': 'right'})
-    seederList = slinfo[::2]
-    leecherList = slinfo[1::2]
+    seeder_list = slinfo[::2]
+    leecher_list = slinfo[1::2]
     seeders = []
     leechers = []
-    for seeder in seederList:
+    for seeder in seeder_list:
         seeders.append(seeder.get_text())
-    for leecher in leecherList:
+    for leecher in leecher_list:
         leechers.append(leecher.get_text())
-    # torrents
-    torrents = []
-    for (title, magnetLink, time, size, uploader, seed, leech) in zip(titles, magnets, times, sizes, uploaders, seeders, leechers):
-        torrents.append({
-            'title': title,
-            'magnet': magnetLink,
-            'time': time,
-            'size': size,
-            'uploader': uploader,
-            'seeds': seed,
-            'leechs': leech,
-        })
-    return jsonify(torrents), 200
+    return seeders, leechers
 
 
 if __name__ == '__main__':
