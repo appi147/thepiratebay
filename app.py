44340d7a6e95d0fbfefa8acf675c416568a3ad13
brasseur <brasseur@sentrysoftware.com>
2020-05-08 13:41:23 -0700

    dont mix tabs and spaces

--- app.py
+++ app.py
@@ -140,11 +140,11 @@ def search_torrents(term=None):
 def parse_page(url, sort=None):
     
     chrome_options = webdriver.ChromeOptions()
-	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-	chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.add_argument("--no-sandbox")
-	
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     driver.get(url)
